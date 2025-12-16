7135c833efc9d871df3061c034dc982e19d577bd
Richard To <31784865+richardrto@users.noreply.github.com>
2025-12-15 14:40:54 -1000

    Update webpack config set up to use merge

--- webpack.common.js
+++ webpack.common.js
@@ -1,18 +1,15 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+ const path = require('path');
+ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    devtool: 'eval-source-map',
-    devServer: {
-        watchFiles: ['./src/template.html'],
-    },
+ module.exports = {
+   entry: {
+     app: './src/index.js',
+   },
+   output: {
+     filename: 'main.js',
+     path: path.resolve(__dirname, 'dist'),
+     clean: true,
+   },
     plugins: [
         new HtmlWebpackPlugin({
             template: './src/template.html',
@@ -38,4 +35,4 @@ module.exports = {
             },
         ],
     },
-};
+ };
